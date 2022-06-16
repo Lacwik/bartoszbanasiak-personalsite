0035680ec18b89e1d05fe205f7d85bab0edaba9b
Lacwik <maciej.lacwik@gmail.com>
2022-06-17 01:56:46 +0200

    working - versions update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -9,8 +11,16 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'bartoszbanasiak-personalsite';
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService,
+              private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer) {
+
     translate.setDefaultLang('pl');
     translate.use('pl');
+
+    this.matIconRegistry.addSvgIcon(
+      `example`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/example.svg")
+    );
   }
 }
