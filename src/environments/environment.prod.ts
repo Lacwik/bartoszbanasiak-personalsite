7c1643efcf72b77670913a57719daf349c023842
Lacwik <maciej.lacwik@gmail.com>
2022-06-23 14:23:16 +0200

    env prod update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,11 +10,11 @@ export const environment = {
   urlInstagram: "https://www.instagram.com/bb_trening_zgierz",
   urlFacebook: "https://www.facebook.com/Bartosz-Banasiak-Trener-Personalny-101043109309262",
 
-  imgBackground1: '../assets/images/background1.jpg',
-  imgBackground2: "../assets/images/background2.jpg",
-  imgBackground3: "../assets/images/background3.jpg",
-  logoPath: "../assets/images/logo.jpg",
-  repLogoPath: "../assets/images/replogo.png",
-  imgProfile: "../assets/images/profilepic.jpg"
+  imgBackground1: './assets/images/background1.jpg',
+  imgBackground2: "./assets/images/background2.jpg",
+  imgBackground3: "./assets/images/background3.jpg",
+  logoPath: "./assets/images/logo.jpg",
+  repLogoPath: "./assets/images/replogo.png",
+  imgProfile: "./assets/images/profilepic.jpg"
 
 };
